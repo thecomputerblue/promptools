f87a063decbc2ff8a99ab9691bcba3702f1d871f
jfeitzinger <jfeitzinger@gmail.com>
2022-05-03 11:08:49 -0400

    generate pp documents
    successfully dumped a string and some random bookmarks into a presentation prompter document. next, get it to apply some generic formatting that is broadly appropriate for teleprompting (white monospace font, size... 80?). then plug into the text export thing... gonna have to make my own bookmark tag...

--- app/tools/ppinterface.py
+++ app/tools/ppinterface.py
@@ -1,20 +1,7 @@
-import plistlib as pll
-import struct
-
-def rawbytes(s):
-    """Convert a string to raw bytes without encoding"""
-    outlist = []
-    for cp in s:
-        num = ord(cp)
-        if num < 255:
-            outlist.append(struct.pack('B', num))
-        elif num < 65535:
-            outlist.append(struct.pack('>H', num))
-        else:
-            b = (num & 0xFF0000) >> 16
-            H = num & 0xFFFF
-            outlist.append(struct.pack('>bH', b, H))
-    return b''.join(outlist)
+import plistlib
+import base64
+import pandoc
+import os
 
 # writing some stuff to interface with presentation prompter
 
@@ -24,59 +11,57 @@ def rawbytes(s):
 
 # defining some basic stuff I see in these plist files
 
-# probably don't need this if i use plistlib but leaving fo rnow
-HEADER = \
-"""<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">"""
-
-def token():
-	def raw():
-		return \
-		"YnBsaXN0MDDUAQIDBAUGFxhYJHZlcnNpb25YJG9iamVjdHNZJGFy" + \
+# PP plist files all contain this. not sure what it is but i'm including it.
+RAW_TOKEN = "YnBsaXN0MDDUAQIDBAUGFxhYJHZlcnNpb25YJG9iamVjdHNZJGFy" + \
 		"Y2hpdmVyVCR0b3ASAAGGoKQHCA8QVSRudWxs0wkKCwwNDlRuYW1l" + \
 		"VHR5cGVWJGNsYXNzgAIQAIADUNIREhMUWiRjbGFzc25hbWVYJGNs" + \
 		"YXNzZXNaUFBCb29rbWFya6IVFlpQUEJvb2ttYXJrWE5TT2JqZWN0" + \
 		"XxAPTlNLZXllZEFyY2hpdmVy0RkaVHJvb3SAAQgRGiMtMjc8QklO" + \
 		"U1pcXmBhZnF6hYiTnK6xtgAAAAAAAAEBAAAAAAAAABsAAAAAAAAA" + \
 		"AAAAAAAAAAC4"
-	return rawbytes(raw())
+
+# need to decode the token and re-encode to get it in plist file correctly.
+TOKEN = base64.b64decode(RAW_TOKEN)
 
 # TODO: dataclasses?
 class PPPropertyList:
 	"""Struct replicating pp plist hierarchy in python."""
 
-	def __init__(self):
-		self.data = {
-	"appBuildNumber" : 722,
-	"appVersion" : "5.8.1",
-	"bookmarks" : PPBookmarks().get(),
-	"extendedMetadata": PPExtendedMetadata().get(),
-	"fileFormatNumber": 5,
-	"scrollbarPosition": 0.0,
-	"selectedRanges": ["{0, 0}"],
-	"speed": 5.9999999999999973,
-	"systemVersion": "10.12.5",
-	"windowFrame": "{{80, 87}, {1360, 790}}"
-	}
+	def __init__(self, *args, **kwargs):
+		self.bookmarks = PPBookmarks()
+		self.extendedMetadata = PPExtendedMetadata()
+		self.speed = 5.9999999999999973
 
 	def get(self):
-		return self.data
+		"""Return the dict to make the plist file."""
+		return {
+		"appBuildNumber" : 722,
+		"appVersion" : "5.8.1",
+		"bookmarks" : self.bookmarks.get(),
+		"extendedMetadata": self.extendedMetadata.get(),
+		"fileFormatNumber": 5,
+		"scrollbarPosition": 0.0,
+		"selectedRanges": ["{0, 0}"],
+		"speed": self.speed,
+		"systemVersion": "10.12.5",
+		"windowFrame": "{{80, 87}, {1360, 790}}"
+		}
+
 
 class PPBookmarks:
 	"""Struct replicating the pp bookmark dict in Properties.plist"""
 
-	def __init__(self):
+	def __init__(self, *args, **kwargs):
 		# store locations as list of ints. int is the absolute character
 		# value of of the bookmark location. 
-		self._locations = [1, 2, 3]
+		self._locations = kwargs.get('locations') if kwargs.get('locations') else [1, 2]
 
 	@property 
 	def data(self):
 		"""I have no idea what this is but it is in any .plist file
 		with bookmarks."""
 		# TODO: not sure if this needs to be explicitly formatted a certain way...
-		return token()
+		return bytes(TOKEN)
 
 	@property
 	def locations(self):
@@ -90,11 +75,12 @@ class PPBookmarks:
 		"locations" : self.locations,
 		}]
 
+
 class PPExtendedMetadata:
 	def __init__(self):
 		# leave empty, think inline images would go in here
 		self.attachmentNames = []
-		# array of strings
+		# array (in python just a list) of strings
 		self.bookmarkNames = []
 
 	def get(self):
@@ -103,19 +89,51 @@ class PPExtendedMetadata:
 		"bookmarkNames": self.bookmarkNames
 		}
 
-bookmarks = PPBookmarks()
-extended_metadata = PPExtendedMetadata()
 
+# TESTING BELOW
+package = "TEST_PACKAGE.ppdocument"
+content = package + "/Content.rtfd"
 
-file = "testdump"
+def make_pp_package():
+	make_pp_outer_package()
+	make_content_wrapper()
 
-def main():
+def make_pp_outer_package():
+	if not os.path.exists(package):
+	    os.mkdir(package)	
+
+def make_content_wrapper():
+	if not os.path.exists(content):
+	    os.mkdir(content)
+
+def dump_test_props():
+	file = package + "/Properties.plist"
 	props = PPPropertyList()
 	with open(file, 'wb') as fp:
-		pll.dump(props.get(), fp)
+		plistlib.dump(props.get(), fp)
 
-main()
+def dump_test_str():
+	file = content + "/TXT.rtf"
+	text = "Proof of concept, exporting to rtf from string"
+	doc = pandoc.read(text)
+	pandoc.write(doc=doc, file=file)
+
+def dump_test_pkg():
+	file = "PKG_TEST.ppdocument"
+	with open(file, 'w') as f:
+		pass
+
+
+def main():
+	make_pp_package()
+	dump_test_props()
+	dump_test_str()
+
+if __name__ == "__main__":
+	main()
 
 """
-plistlib documentation; https://docs.python.org/3/library/plistlib.html
+plistlib: https://docs.python.org/3/library/plistlib.html
+pandoc: https://boisgera.github.io/pandoc
+apple document packages: https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFBundles/DocumentPackages/DocumentPackages.html
 """
